f237f27e100280ecf6bc9cfbbe62825270dc1e46
Vishwas Pandey <Vishwaspandey440@gmail.com>
2025-10-31 18:21:03 +0530

    chore: Final production link update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://hr-management-system-6a3b.onrender.com",
 });
 
 // This is the interceptor. It runs BEFORE every request.
